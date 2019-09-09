0c3aae59d470c983e40b72890404d891a9df7826
xiaodongbei <xiaodongbeij@gmail.com>
2019-09-09 16:58:12 +0800

    显示个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+
 
 class UsersController extends Controller
 {
@@ -13,13 +15,14 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
-    public function edit()
+    public function edit(User $user)
     {
-        return view('users.edit');
+        return view('users.edit', compact('user'));
     }
 
-    public function update()
+    public function update(UserRequest $request, User $user)
     {
-
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
