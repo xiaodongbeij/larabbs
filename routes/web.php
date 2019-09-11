b62b9a1fe87add8d69d4bbc35cbc59639e1d4967
xiaodongbei <xiaodongbeij@gmail.com>
2019-09-11 14:01:40 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Auth::routes(['verify' => true]);
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
